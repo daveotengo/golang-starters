15c630dd4d9fb3fc6c55ff988d8410223a9f86f5
David <daveotengo@yahoo.com>
2024-02-24 14:43:51 +0000

    displaying int and string values in a main function

--- main.go
+++ main.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	var ageOne = 12
+	var ageTwo = 23
+	ageThree := 34
 
-	fmt.Println("Hello")
+	var nameOne = "Ama"
+	var nameTwo = "Adjoa"
+	nameThree := "Kwasi"
+
+	//bits and memory
+	var numOne int8 = 127
+	var numTwo int8 = -128
+	var numThree uint16 = 25694
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	nameThree = "Yaa"
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	fmt.Println(numOne, numTwo, numThree)
+
 }

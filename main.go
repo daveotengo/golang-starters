d250940f314ba68dd632d8635fe272e1a7e4292c
David <daveotengo@yahoo.com>
2024-02-26 21:26:30 +0000

    multi return values

--- main.go
+++ main.go
@@ -1,34 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"strings"
 )
 
-func sayGreeting(n string) {
-	fmt.Printf("Good morning %v \n", n)
-}
-
-func sayBye(n string) {
-	fmt.Printf("Goodbye %v \n", n)
-}
-
-func cycleNames(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
 	}
-}
 
-func circleArea(r float64) float64 {
-	return math.Pi * r * r
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+	return initials[0], "_"
 }
 
 func main() {
-	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
-	cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
-
-	a1 := circleArea(10.5)
-	a2 := circleArea(15)
-	fmt.Println(a1, a2)
-
+	getInitials("David Oteng")
 }

f4f35a1609fc345aca33de119a99051cdae92b8f
David <daveotengo@yahoo.com>
2024-02-27 06:14:54 +0000

    passby

--- main.go
+++ main.go
@@ -2,29 +2,14 @@ package main
 
 import "fmt"
 
+func updateName(x string) string {
+	x = "wedge"
+	return x
+}
+
 func main() {
-	menu := map[string]float64{
-		"soup":           4.99,
-		"pie":            7.99,
-		"salad":          6.99,
-		"toffee pudding": 3.55,
-	}
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
+	name := "tifa"
+	name = updateName(name)
 
-	for k, v := range menu {
-		fmt.Println(k, "-", v)
-	}
-
-	phoneBook := map[int]string{
-		123213123: "David",
-		243243242: "Kwadwo",
-		546456454: "Emmanuel",
-		122324233: "Reginald",
-	}
-
-	fmt.Println(phoneBook)
-	phoneBook[243243242] = "Abena"
-	fmt.Println(phoneBook)
-
+	fmt.Println(name)
 }
